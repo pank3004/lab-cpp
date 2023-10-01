24996c2638dfc814a560acdc3f4febbeb39cb715
pank3004 <pankajkumarewpk2004@gmail.com>
2023-10-01 09:11:05 +0530

    ccccomitt

--- pankaj/pop.c
+++ pankaj/pop.c
@@ -27,6 +27,7 @@ int main()
             printf("Element is %d\n",arr[top]);
         }
     }
+    //ghbhjkjkhlhuihkjgsertwhgdfgs
 
     return 0;
 }
--- pankaj/push.c
+++ pankaj/push.c
@@ -22,4 +22,5 @@ int main()
     //     printf("%d\n",arr[i]);
     // }
     return 0;
+    ////////
 }
